83a0bbcef3cd6a50e20381ad059ef19ba4f6438f
Sergey <sb.truefalse@gmail.com>
2020-04-18 18:36:42 +0700

    [+] Locales

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
-  
+  around_action :switch_locale
+
   private
+
+  def switch_locale(&action)
+    puts request.base_url, request.original_fullpath, "#####3", request.env['PATH_INFO']
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 
   def have_access?
     if session[:secret_token] == ENV['CONTROLLER_SECRET_TOKEN']
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'schedules#index'
-  resources :auth, only: [:new, :create]
-  resources :schedules, only: [:index, :show]
-  resources :occupations, only: [:index, :show, :new, :create,:destroy]
+  get '/:locale' => 'schedules#index'
+
+  scope "(:locale)", locale: /en|ru/ do
+    resources :auth, only: [:new, :create]
+    resources :schedules, only: [:index, :show]
+    resources :occupations, only: [:index, :show, :new, :create,:destroy]
+  end
 end
